d5f4c7d9e89f3e90ae3eb1f60beaf6ed7ef5d702
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-12-16 15:44:10 -0300

    utilizando basicamente flask_httpauth

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    return 'admin'
 
 @app.route('/', methods=["GET"])
+@auth.login_required
 def hello_world():
-    return "Oxigenai Gaba"
+    return f"Oxigenai Gaba (user {auth.current_user()})"
 
 if __name__ == '__main__':
     app.run()
