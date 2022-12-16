7bf2d2ab50407729b00a5f46198e0eb3bb5b5819
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-12-16 16:41:10 -0300

    werkzeug check_password_hash

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+from pprint import pprint
+
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+users = {
+    "admin": generate_password_hash("234234",'pbkdf2:sha512:8000', 64),
+}
 
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+
 @auth.verify_password
 def verify_password(username, password):
-    return 'admin'
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 
 @app.route('/', methods=["GET"])
 @auth.login_required
 def hello_world():
     return f"Oxigenai Gaba (user {auth.current_user()})"
+
 
 if __name__ == '__main__':
     app.run()
